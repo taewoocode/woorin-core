fbb4eed69f37a04d92045196705712e0ba71323f
taewoocode <qkrxodn6035@gmail.com>
2026-08-25 21:55:33 +0900

    first commit

---
+++ src/main/java/com/example/woorincore/WoorinCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.woorincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorinCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorinCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/woorincore/WoorinCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woorincore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorinCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
